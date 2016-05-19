9b869c0adecef7e3b28e4e17442f0cd82344e6a6
Eric Wehrly <eric@disruptiontheory.com>
2016-05-18 23:56:17 -0400

    Initial commit

---
+++ CoOpTowerDefense/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CoOpTowerDefense
+{
+    class Program
+    {
+        private static bool _shutDownIssued = false;
+
+        static void Main(string[] args)
+        {
+
+            StartGameServer();
+        }
+
+        private static void StartGameServer()
+        {
+            const string nancyUri = "http://localhost:1337/";
+
+            /*
+            using (var host = new NancyHost(new Uri(nancyUri)))
+            {
+                host.Start();
+#if DEBUG
+                Process.Start(nancyUri);
+#endif
+
+                Console.ReadLine();
+
+                host.Stop();
+
+                _shutDownIssued = true;
+            }
+            */
+            Console.ReadLine();
+        }
+
+        private static void StartGameLoop()
+        {
+            while (!_shutDownIssued)
+            {
+                // PhysicsEngine.MainLoop();
+            }
+        }
+    }
+}
