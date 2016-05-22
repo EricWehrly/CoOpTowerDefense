429ee729e455c76b16fb3b9b6cf0ed0bca1f1081
Eric Wehrly <eric@disruptiontheory.com>
2016-05-22 03:01:37 -0400

    Added client project

---
+++ CoOpClient/Network/JQueryAjaxNetworkConnector.cs
@@ -0,0 +1,39 @@
+﻿using SharpKit.JavaScript;
+using SharpKit.jQuery;
+
+namespace CoOpClient.Network
+{
+    [JsType(JsMode.Clr, Filename = "../res/Network.js")]
+    public class JQueryAjaxNetworkConnector : IClientNetworkConnector
+    {
+        public void SendCommand(string command, object data)
+        {
+            var ajaxSettings = new AjaxSettings
+            {
+                url = "http://localhost:1337/" + command,
+                cache = false,
+                data = data,
+                dataType = "",
+                // success = Success
+            };
+
+            jQuery.ajax(ajaxSettings);
+        }
+
+        public object SendQuery(string query, object data)
+        {
+            var ajaxSettings = new AjaxSettings
+            {
+                url = "http://localhost:1337/" + query,
+                cache = false,
+                data = data,
+                dataType = "",
+                // success = Success
+            };
+
+            jQuery.ajax(ajaxSettings);
+
+            return "";
+        }
+    }
+}
